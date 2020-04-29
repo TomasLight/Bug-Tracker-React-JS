fa2d53e4c19f251ed875a8c505c85412c2c583da
Tomas_Light <pikachu-94@yandex.ru>
2020-04-29 13:41:43 +0300

    bugfix form validation

--- Client/ClientApp/src/shared/organisms/Form/Form.tsx
+++ Client/ClientApp/src/shared/organisms/Form/Form.tsx
@@ -1,4 +1,6 @@
-import { FormSubscription, ValidationErrors } from "final-form";
+import { FormApi, FormSubscription, SubmissionErrors } from "final-form";
+import { setErrors } from "final-form-set-errors-mutator";
+import { IValidator } from "model-state-validation";
 import React, { FunctionComponent, KeyboardEvent } from "react";
 import { Form as FinalForm } from "react-final-form";
 
@@ -6,30 +8,80 @@ export interface IAppFormProps<FormValues = any> {
     submit: (formValues: FormValues) => void;
     initialValues?: FormValues;
     className?: any;
-    validate?: (
-        values: FormValues
-    ) => ValidationErrors | Promise<ValidationErrors> | undefined;
-    validateOnBlur?: boolean;
+    validator?: IValidator<FormValues>;
 }
 
 type Props = IAppFormProps;
 
 const Form: FunctionComponent<Props> = (props) => {
-    const { submit, children, className, ...rest } = props;
+    const { submit, children, className, validator, ...rest } = props;
+
+    let formApi: FormApi = null;
+    let wasSubmit = false;
 
     const formSubscription: FormSubscription = {
         submitting: true,
+        dirtyFieldsSinceLastSubmit: true,
+    };
+
+    const onSubmit = (formValues: any) => {
+        wasSubmit = true;
+        if (!validator) {
+            submit(formValues);
+            return;
+        }
+
+        const modelState = validator.validate(formValues);
+        if (modelState.isInvalid()) {
+            formApi.pauseValidation();
+
+            formApi.mutators.setErrors(modelState);
+
+            if (formApi.isValidationPaused()) {
+                formApi.resumeValidation();
+            }
+            return;
+        }
+
+        submit(formValues);
+    };
+
+    const validate = (formValues: any) => {
+        if (!validator || !formApi) {
+            return;
+        }
+
+        const formState = formApi.getState();
+        if (formState.pristine && !formState.submitting || !wasSubmit) {
+            return;
+        }
+
+        const modelState = validator.validate(formValues);
+        const errors = modelState.getErrors();
+
+        if (formState.submitting) {
+            return errors;
+        }
+
+        errors[formState.active] = undefined;
+        return errors;
     };
 
     return (
         <FinalForm
-            onSubmit={submit}
+            onSubmit={onSubmit}
+            validate={validate}
             subscription={formSubscription}
+            mutators={{
+                setErrors,
+            }}
             {...rest}
         >
-            {({ handleSubmit }) => {
+            {({ handleSubmit, form }) => {
+                formApi = form;
+
                 const onKeyPress = (event: KeyboardEvent) => {
-                    if (event.key === "Enter") {
+                    if (event.key === "Enter" && !event.shiftKey) {
                         handleSubmit();
                     }
                 };
