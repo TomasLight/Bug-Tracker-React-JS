18618ecee5b8a58cb12d6b3385df235579c850d0
TomasLight <art.ser.ignatiev@gmail.com>
2020-06-20 23:58:31 +0300

    rename solution

--- API/Security/AuthOptions.cs
+++ API/Security/AuthOptions.cs
@@ -8,7 +8,7 @@ namespace API.Security
 		private static readonly string EncryptionKey = "U4MV%EFzQ8j5ok#tv4m2ntY28WJRZ9J5s!LoPiDLtLKa4ebaM^IJH1se%3XD";
 
 		public static readonly string Issuer = "Tomas_Light";
-		public static readonly string Audience = "Bugs.API";
+		public static readonly string Audience = "issue-tracker-api";
 
 		// 12 hours
 		public static readonly int LifeTimeInMinutes = 720;
